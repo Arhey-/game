7362d94713ee2fc44c1eee7d72a089f490a0e597
Arhey- <Arhey-@users.noreply.github.com>
2015-01-26 00:19:00 +0200

    Update drive.js

--- drive.js
+++ drive.js
@@ -1,3 +1,45 @@
+function searchFile(name, callback) {
+  name = name || 'mus.json';
+  gapi.client.load('drive', 'v2', function() {
+    var q = gapi.client.drive.files.list({'q': 'title = \'' + name + '\''});
+    q.execute(function (resp) {
+      var id = (resp.items.length) ? resp.items[0].id : '';
+      callback(id);
+    });
+  });
+}
+
+function getFile(id) {
+	if (id.length) {
+		printFile(id);
+	} else {
+		alert('insertFile');
+	}
+}
+
+/**
+ * Print a file's metadata.
+ *
+ * @param {String} fileId ID of the file to print metadata for.
+ */
+function printFile(fileId) {
+  var request = gapi.client.drive.files.get({
+      'id': fileId
+  });
+  request.execute(function(resp) {
+    if (!resp.error) {
+      console.log('Title: ' + resp.title);
+      console.log('Description: ' + resp.description);
+      console.log('MIME type: ' + resp.mimeType);
+    } else if (resp.error.code == 401) {
+      // Access token might have expired.
+      checkAuth();
+    } else {
+      console.log('An error occured: ' + resp.error.message);
+    }
+  });
+}
+
 /**
  * Copy an existing file.
  *
